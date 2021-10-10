e14d47ca29faef20dab758ee5700f08d316816d5
Unknown <achrafatauil@hotmail.com>
2021-10-10 12:05:50 +0200

    create config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+const APPLICATION_GRAPH = process.env.SESSIONS_GRAPH || "http://mu.semte.ch/application";
+const FILE_RESOURCE_BASE = process.env.FILE_RESOURCE_BASE || "http://mu.semte.ch/services/file-service/files/";
+const BUCKET_NAME = process.env.BUCKET_NAME;
