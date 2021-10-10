72faa13a51ed681102e23b64ce95a1e2d82e8ef6
Unknown <achrafatauil@hotmail.com>
2021-10-10 13:03:15 +0200

    use env vars + create test for connection

--- s3.js
+++ s3.js
@@ -1,12 +1,29 @@
+import { ACCESS_KEY_ID, BUCKET_ENDPOINT, BUCKET_NAME, BUCKET_REGION, SECRET_ACCESS_KEY } from './config';
+
 var AWS = require('aws-sdk');
 
-var s3  = new AWS.S3({
-          accessKeyId: 'minio' ,
-          secretAccessKey: 'minio123' ,
-          endpoint: 'minio:9000' ,
-          s3ForcePathStyle: true,
-          sslEnabled: false,
-          signatureVersion: 'v4'
+const params = {
+  accessKeyId: ACCESS_KEY_ID ,
+  secretAccessKey: SECRET_ACCESS_KEY ,
+  endpoint: BUCKET_ENDPOINT ,
+  s3ForcePathStyle: true,
+  sslEnabled: false,
+  signatureVersion: 'v4'
+}
 
-});
+if (BUCKET_REGION) {
+  params[region] = BUCKET_REGION
+}
+
+const s3 = new AWS.S3(params)
+
+// Test if we can access specified bucket
+if(s3) {
+  s3.listObjects({Bucket: BUCKET_NAME}, (err) => {
+    if(err != null) {
+      console.log(err)
+    }
+  });
+}
+
 export { s3 }
